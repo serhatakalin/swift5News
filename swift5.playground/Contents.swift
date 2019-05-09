8ce6c264fc96243e845d7d3e9384964730b41ef9
Serhat <akalinst@gmail.com>
2019-05-09 12:56:49 +0300

    interpolation, unknown default,

--- swift5.playground/Contents.swift
+++ swift5.playground/Contents.swift
@@ -57,3 +57,41 @@ if num.isMultiple(of: 2) {
     print("num is odd")
 }
 
+//string interpolation
+struct Product {
+    var name:String
+    var barcode:String
+    var id:Int
+}
+
+extension String.StringInterpolation {
+    mutating func appendInterpolation(_ value: Product) {
+        appendInterpolation("Product name -> \(value.name) & id -> \(value.id)")
+    }
+    
+    mutating func appendInterpolation(_ number: Int, val: Int) {
+        let result = String(number + val)
+        appendLiteral(result)
+    }
+}
+
+let productObject = Product(name: "Cap", barcode: "810573746552", id: 93023)
+print("Product Details: \(productObject)")
+
+let x = 60
+print("\(x, val: 2)") // 62
+
+//@unknown default in enums
+enum animals {
+    case cat
+    case dog
+    case lion
+}
+
+let animalsObj:animals = .cat
+
+switch animalsObj {
+case .cat: print("cat")
+case .dog: print("dog")
+@unknown default: print("unknown")
+}
